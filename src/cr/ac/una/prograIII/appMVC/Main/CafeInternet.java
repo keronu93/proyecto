61435c987948fe6c4e3073cf389642851b4c28d8
guroja1 <grcantill@gmail.com>
2016-10-03 22:50:49 -0600

    Merge origin/master
    Conflicts:
    	src/cr/ac/una/prograIII/appMVC/Vista/Server.java

--- src/cr/ac/una/prograIII/appMVC/Main/CafeInternet.java
+++ src/cr/ac/una/prograIII/appMVC/Main/CafeInternet.java
@@ -31,25 +31,25 @@ public class CafeInternet {
     public static void main(String[] args) {
 
         
-         InterFazSistemaAdministracion manteAdmiView =new InterFazSistemaAdministracion();
-          ArticuloBL articuloBlModelo= new ArticuloBL();
-          ClienteBL clienteBlModelo= new ClienteBL();
-          ProveedorBL proveedorBlModelo= new ProveedorBL();
-          TelefonoBL telefonoBlModelo=new TelefonoBL();
-          UsuarioBL usuarioBLModelo = new UsuarioBL();
-          ControladorSistAdministracion admiControlador= new ControladorSistAdministracion(manteAdmiView,articuloBlModelo,clienteBlModelo,proveedorBlModelo,telefonoBlModelo,usuarioBLModelo);
-    
-          admiControlador.getManteAdmiView().setVisible(true);
-          ArrayList<ServerControlador.ClienteHilo> listaClientes =new ArrayList<>();
-          Server manteServerView = new Server();
-          ServerControlador serControlador = new  ServerControlador(manteServerView,listaClientes);
-          serControlador.getServerView().setVisible(true);
-          System.out.println("prueba");
-//            SolicitudUsuarioPassword solUsuarioPassword =new SolicitudUsuarioPassword();
-//            InterFazSistemaAdministracion ManteAdmiView= new InterFazSistemaAdministracion();
-//            SolicitudUsuarioPassword solUsePa = new SolicitudUsuarioPassword();
-//            ValidarAcceso validar = new ValidarAcceso(ManteAdmiView, solUsePa);
-//            validar.getSolUsePa().setVisible(true);
+//         InterFazSistemaAdministracion manteAdmiView =new InterFazSistemaAdministracion();
+//          ArticuloBL articuloBlModelo= new ArticuloBL();
+//          ClienteBL clienteBlModelo= new ClienteBL();
+//          ProveedorBL proveedorBlModelo= new ProveedorBL();
+//          TelefonoBL telefonoBlModelo=new TelefonoBL();
+//          UsuarioBL usuarioBLModelo = new UsuarioBL();
+//          ControladorSistAdministracion admiControlador= new ControladorSistAdministracion(manteAdmiView,articuloBlModelo,clienteBlModelo,proveedorBlModelo,telefonoBlModelo,usuarioBLModelo);
+//    
+//          admiControlador.getManteAdmiView().setVisible(true);
+//          ArrayList<ServerControlador.ClienteHilo> listaClientes =new ArrayList<>();
+//          Server manteServerView = new Server();
+//          ServerControlador serControlador = new  ServerControlador(manteServerView,listaClientes);
+//          serControlador.getServerView().setVisible(true);
+//          System.out.println("prueba");
+            SolicitudUsuarioPassword solUsuarioPassword =new SolicitudUsuarioPassword();
+            InterFazSistemaAdministracion ManteAdmiView= new InterFazSistemaAdministracion();
+            SolicitudUsuarioPassword solUsePa = new SolicitudUsuarioPassword();
+            ValidarAcceso validar = new ValidarAcceso(ManteAdmiView, solUsePa);
+            validar.getSolUsePa().setVisible(true);
             
                
     }
